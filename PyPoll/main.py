7ed312cf65b1750a4613b51542de4956956c1497
JackXinPan <79770660+JackXinPan@users.noreply.github.com>
2021-09-07 03:31:44 +0800

    added readme

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,6 +5,7 @@ import csv
 dirname = os.path.dirname(__file__)
 data = os.path.join(dirname,"Resources", "election_data.csv")
 
+
 #variable to hold total votes
 total = 0
 
@@ -30,23 +31,33 @@ with open(data) as csvfiles:
             votecount[row[2]] = votecount[row[2]] + 1
 
 #Printing Output
-print("Election Results")
-print("-------------------------")
-print(f"Total Votes: {total}")
-print("-------------------------")
-#variable to adjudicate the winner of the election
-winner = ""
-#loop to print results
-for x in votecount:
-    # Print results, converting vote percentage using the round function
-    print(f'{x}: {round(100 * votecount[x] / total, 3):.3f}% ({votecount[x]})')
-    #conditional loop to find the winner
-    if winner == "":
-        winner = x
-    elif votecount[x] >= votecount[winner]:
-        winner = x 
-print('-------------------------')
-print(f'Winner: {winner}')
-print('-------------------------')
+def analysis():
+    print("Election Results")
+    print("-------------------------")
+    print(f"Total Votes: {total}")
+    print("-------------------------")
+    #variable to adjudicate the winner of the election
+    winner = ""
+    #loop to print results
+    for x in votecount:
+        # Print results, converting vote percentage using the round function
+        print(f'{x}: {round(100 * votecount[x] / total, 3):.3f}% ({votecount[x]})')
+        #conditional loop to find the winner
+        if winner == "":
+            winner = x
+        elif votecount[x] >= votecount[winner]:
+            winner = x 
+    print('-------------------------')
+    print(f'Winner: {winner}')
+    print('-------------------------')
 
-
+#terminal
+print(analysis())
+#path to write the results 
+main_analysis = os.path.dirname(__file__)
+passageFile = os.path.join(dirname, "mainanalysis.txt")
+#write results into text file
+outF = open(main_analysis,'w')
+#write using writelines
+outF.writelines(analysis())
+outF.close()
